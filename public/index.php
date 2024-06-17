33d69209eb187163aea979029a9dbfa55fe31fdc
ao <aolin@ymail.com>
2024-06-17 21:24:07 +0300

    router changes'

--- public/index.php
+++ public/index.php
@@ -4,4 +4,7 @@ require '../helpers.php';
 // loadView('home');
 
 $uri=$_SERVER['REQUEST_URI'];
+
+$method = $_SERVER['REQUEST_METHOD'];
+
 require basePath('router.php');
--- router.php
+++ router.php
@@ -1,11 +1,45 @@
 <?php
 
-$routes = require basePath('routes.php');
+// $routes = require basePath('routes.php');
 
-inspect($uri);
-if(array_key_exists($uri,$routes)){
-    require (basePath($routes[$uri]));
-}else{
-    http_response_code(404);
-    require basePath($routes['404']);
+// inspect($uri);
+// if(array_key_exists($uri,$routes)){
+//     require (basePath($routes[$uri]));
+// }else{
+//     http_response_code(404);
+//     require basePath($routes['404']);
+// }
+
+class Router{
+    protected $routes = [];
+
+    /** GET ROUTE
+     * @param string $uri
+     * @param string $controller
+     * return void
+     */
+    public function get($uri,$controller){
+        $this->$routes;
+    }
+
+        /** POST ROUTE
+     * @param string $uri
+     * @param string $controller
+     * return void
+     */
+    public function post($uri,$controller){}
+
+        /** PUT ROUTE
+     * @param string $uri
+     * @param string $controller
+     * return void
+     */
+    public function put($uri,$controller){}
+
+        /** DELETE ROUTE
+     * @param string $uri
+     * @param string $controller
+     * return void
+     */
+    public function delete($uri,$controller){}
 }
