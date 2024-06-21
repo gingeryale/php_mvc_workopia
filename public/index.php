3b446921feab9f4e2dc9db343356fef24a69501d
ao <aolin@ymail.com>
2024-06-21 18:49:10 +0300

    spl_autoload_register function

--- public/index.php
+++ public/index.php
@@ -3,8 +3,17 @@
 require '../helpers.php';
 // $config = require basePath('config/db.php');
 // $db = new Database($config);
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
+
+// spl_autoload replaced these
+// require basePath('Framework/Router.php');
+// require basePath('Framework/Database.php');
+
+spl_autoload_register(function($class){
+    $path=  basePath('Framework/'. $class . '.php');
+    if(file_exists($path)){
+        require $path;
+    }
+});
 
 // Instantiate
 $router = new Router();
