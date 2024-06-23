7bc2b86b83adef36fe1fb0f5ea53e97990a47ed3
ao <aolin@ymail.com>
2024-06-23 22:30:15 +0300

    created validate class to check for input

---
+++ Framework/Validation.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Framework;
+
+class Validation{
+    /**
+     * Validation string
+     * @param int $min
+     * @param int $max
+     * @return bool
+     */
+
+     public static function string ($value,$min=1,$max=INF){
+        if(is_string($vlaue)){
+            $value = trim($value);
+            $length = strlen($value);
+            return $length >= $min && $length <= $max;
+        }
+        return false;
+     }
+
+     /** Validate email
+      * @param string $value
+      * @return mixed
+      */
+      public static function email($value){
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+      }
+
+      /** 
+       * Match and compare 2 values
+       * @param string $value
+       * @return bool
+       */
+      public static function match($value1,$value2){
+        $value1 = trim($value1);
+        $value2 = trim($value2);
+        return $value1 === $value2;
+      }
+}
